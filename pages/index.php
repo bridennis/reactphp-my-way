ebf25ecb0edc95858a290de04188254259be9fb1
dbritousov <d.britousov@estp.ru>
2018-12-28 16:25:12 +0300

    List of files released

---
+++ pages/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Список загруженных файлов</title>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <style>
+        body { padding-top: 50px }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Загрузка файла</h1>
+        </div>
+        <div class="col-sm-12">
+            <form action="/upload"
+                  method="post"
+                  class="justify-content-center"
+                  enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="file">Загрузить файл:</label>
+                    <input name="file"
+                           id="file"
+                           type="file"
+                           accept="image/x-png,image/jpeg"
+                    />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    Отправить
+                </button>
+            </form>
+        </div>
+        <div class="col-sm-12">
+            <hr>
+            <h3>Уже загруженные файлы:</h3>
+            <?php $uploads = file('php://stdin'); ?>
+            <?php if (empty($uploads)) : ?>
+                Нет файлов.
+            <?php else : ?>
+                <ul class="list-group col-sm-6">
+                    <?php foreach ($uploads as $upload) : ?>
+                        <li class="list-group-item">
+                            <?= $upload; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- routes.php
+++ routes.php
@@ -7,20 +7,6 @@ use React\EventLoop\LoopInterface;
 use React\Http\Response;
 
 return [
-    '/' => function (
-        ServerRequestInterface $request,
-        LoopInterface $loop
-    ) {
-        $childProcess = new Process('cat pages/index.html', __DIR__);
-        $childProcess->start($loop);
-
-        return new Response(
-            200,
-            ['Content-type' => 'text/html; charset=UTF-8'],
-            $childProcess->stdout
-        );
-    },
-
     '/upload' => function (
         ServerRequestInterface $request,
         LoopInterface $loop
@@ -34,10 +20,25 @@ return [
         $process->start($loop);
         $process->stdin->end($file->getStream()->getContents());
 
+        return new Response(302, ['Location' => '/']);
+    },
+
+    '/' => function (
+        ServerRequestInterface $request,
+        LoopInterface $loop
+    ) {
+        $listFiles = new Process('ls uploads', __DIR__);
+        $listFiles->start($loop);
+
+        $renderPage = new Process('php pages/index.php', __DIR__);
+        $renderPage->start($loop);
+
+        $listFiles->stdout->pipe($renderPage->stdin);
+
         return new Response(
             200,
-            ['Content-Type' => 'text/plain; charset=UTF-8'],
-            'Загрузка завершена'
+            ['Content-Type' => 'text/html; charset=UTF-8'],
+            $renderPage->stdout
         );
     }
 ];
