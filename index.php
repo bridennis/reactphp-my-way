898e20cbd646fe41709f84ecfd1f0b6c293b8eba
dbritousov <d.britousov@estp.ru>
2018-12-29 10:16:10 +0300

    Error handling added

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+use App\ErrorHandler;
 use App\Router;
 use React\Http\Server;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,15 +12,27 @@ $loop = React\EventLoop\Factory::create();
 $router = new Router($loop);
 $router->load(__DIR__ . '/routes.php');
 
+$errorHandler = new ErrorHandler($loop);
+
 $server = new Server(
-    function (ServerRequestInterface $request) use ($router) {
-        return $router($request);
+    function (ServerRequestInterface $request) use ($router, $errorHandler) {
+        try {
+            return $router($request);
+        } catch (Throwable $exception) {
+            return $errorHandler->handle($exception);
+        }
     }
 );
 
 $socket = new React\Socket\Server(8080, $loop);
-
 $server->listen($socket);
+
+$server->on('error', function (Exception $exception) {
+    echo 'Error: ' . $exception->getMessage() . PHP_EOL;
+    if ($exception->getPrevious() !== null) {
+        echo 'Error: ' . $exception->getPrevious()->getMessage() . PHP_EOL;
+    }
+});
 
 echo 'Работает на '
     . str_replace('tcp:', 'http:', $socket->getAddress())
---
+++ src/ErrorHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app;
+
+use React\ChildProcess\Process;
+use React\EventLoop\LoopInterface;
+use React\Http\Response;
+use Throwable;
+
+class ErrorHandler
+{
+    private $loop;
+
+    public function __construct(LoopInterface $loop)
+    {
+        $this->loop = $loop;
+    }
+
+    public function handle(Throwable $throwable)
+    {
+        $this->report($throwable);
+        return $this->process($throwable);
+    }
+
+    private function report(Throwable $throwable)
+    {
+        echo 'Error: ' . $throwable->getMessage() . PHP_EOL;
+    }
+
+    private function process(Throwable $throwable)
+    {
+        $process = new Process('cat pages/error.html');
+        $process->start($this->loop);
+        return new Response(
+            500,
+            ['Content-Type' => 'text/html'],
+            $process->stdout
+        );
+    }
+}
